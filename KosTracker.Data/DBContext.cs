83197e7cad065b413d9f4e2dce3dd0538e09e364
apony222 <m.mahadi1994@gmail.com>
2023-11-05 11:29:48 +0100

    +Migrations

--- KosTracker.Data/DBContext.cs
+++ KosTracker.Data/DBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using KosTracker.Core;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,14 @@ namespace KosTracker.Data
 {
     public class DBContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-K8KU634\SQLEXPRESS;Initial Catalog=KOSTRACKERWPF;Integrated Security=True;Encrypt=False");
+        }
 
+       //Add Tables
+       public DbSet<User> Users { get; set; }
+       public DbSet<Kosten> Kosten { get; set; }
+       public DbSet<KostenSoll> kostenSoll {  get; set; }
     }
 }
---
+++ KosTracker.Data/Migrations/20231105102855_AddTabls.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KosTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTabls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kosten",
+                columns: table => new
+                {
+                    Kategorie = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ausgabe = table.Column<float>(type: "real", nullable: false),
+                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "kostenSoll",
+                columns: table => new
+                {
+                    Miete = table.Column<float>(type: "real", nullable: false),
+                    Nebenkosten = table.Column<float>(type: "real", nullable: false),
+                    Lebensmittel = table.Column<float>(type: "real", nullable: false),
+                    Gesundheit = table.Column<float>(type: "real", nullable: false),
+                    Versicherung = table.Column<float>(type: "real", nullable: false),
+                    Transport = table.Column<float>(type: "real", nullable: false),
+                    Bekleidung = table.Column<float>(type: "real", nullable: false),
+                    Unterhaltung = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kosten");
+
+            migrationBuilder.DropTable(
+                name: "kostenSoll");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
