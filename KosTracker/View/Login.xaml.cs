a16e1321b9217512c2d91cf3d1777f6631773600
apony222 <m.mahadi1994@gmail.com>
2023-11-12 15:57:36 +0100

    Login+Home

---
+++ KosTracker/View/Login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KosTracker.View
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            KennwortBox.Visibility = Visibility.Collapsed;
+            KennwortTxt.Visibility = Visibility.Visible;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            KennwortBox.Visibility = Visibility.Visible;
+            KennwortTxt.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Home home = new Home();
+            home.Show();
+        }
+    }
+}
---
+++ KosTracker/ViewModel/LoginViewModel.cs
@@ -0,0 +1,62 @@
+﻿using GalaSoft.MvvmLight.Command;
+using KosTracker.Core;
+using KosTracker.Data;
+using KosTracker.Data.SqlOperations;
+using KosTracker.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace KosTracker.ViewModel
+{
+    abstract class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private readonly IDataHelper<User> dataHelper;
+        private ObservableCollection<User> users;
+        bool isLoggedIn=false;
+
+        ICommand LoginCommand {  get; }
+        
+        public LoginViewModel()
+        {
+            dataHelper = new UserEntity();
+            users = new ObservableCollection<User>();
+            LoadData();
+            //LoginCommand = new RelayCommand();
+        }
+
+        private async void LoadData()
+        {
+            users.Clear();
+            foreach (User user in await dataHelper.GetAllAsync())
+            {
+                users.Add(user);
+            }
+        }
+
+       /* private async bool Login(string UserName, string Password)
+        {
+            
+            LoadData();
+            foreach (User user in await dataHelper.GetAllAsync())
+            {
+                if(user.UserName.Equals(UserName)&&user.Password.Equals(Password))
+                {
+                    isLoggedIn = true;   
+                }
+            }
+            return isLoggedIn;
+        }*/
+    }
+}
